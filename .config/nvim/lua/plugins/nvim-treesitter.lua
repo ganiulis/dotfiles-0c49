fb4a4585a50f06eab65f017d2142c829ef828af9
Gintaras Aniulis <91011496+ganiulis@users.noreply.github.com>
2025-03-17 10:12:58 +0200

    Add render-markdown plugin

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,9 +2,16 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
+      "c",
       "lua",
+      "vim",
+      "vimdoc",
+      "query",
       "markdown",
       "markdown_inline",
+    },
+    highlight = {
+      enable = true,
     },
   },
   config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "DaikyXendo/nvim-material-icon",
+  },
+  opts = {
+    html = { enabled = false },
+    latex = { enabled = false },
+  },
+}
