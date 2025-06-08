3fc9b33414912e2aad2ab339a6a6ad4f0a5ec753
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-06-08 23:06:58 +0300

    Fish config improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
 if status is-interactive
     set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_function_path
-    set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 end
