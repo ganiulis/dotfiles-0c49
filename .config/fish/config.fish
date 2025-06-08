00db8b4e9fcbd3fd89a60fc51f2b2de52b59a646
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-06-08 23:06:37 +0300

    Fish config improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_function_path
+    set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 end
