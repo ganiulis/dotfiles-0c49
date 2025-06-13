0f02f9ebb73bde63c67ad7304cdf2d54adb31e9e
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-06-13 19:50:19 +0300

    Update Fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,7 +1,3 @@
 function fish_prompt
     echo -s (set_color green) (whoami) " " (set_color blue) (prompt_pwd) (set_color red) (fish_git_prompt) " " (set_color normal)"→ "
 end
-
-function fish_right_prompt
-    echo -s (set_color yellow) (__kube_prompt) " " (set_color normal) (date +%T)
-end
---
+++ .config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,3 @@
+function fish_right_prompt
+    echo -s (set_color yellow) (__kube_prompt) " " (set_color normal) (date +%T)
+end
