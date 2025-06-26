a2346678fe0302fb570db8e6882a28f006b11b32
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-06-26 17:24:30 +0300

    Update Neovim colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  opts = {
-    transparent_mode = true,
-  },
-  config = function(_, opts)
-    vim.o.background = "dark"
-    require("gruvbox").setup(opts)
-    vim.cmd.colorscheme("gruvbox")
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_transparent_background = 2
+    vim.g.gruvbox_material_foreground = "mix"
+    vim.cmd.colorscheme("gruvbox-material")
   end,
 }
