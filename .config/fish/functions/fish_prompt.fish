6916e6175d42ce8cd8e8989f3b310ecad6b7e051
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-07-15 00:26:13 +0300

    Update Fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-    echo -s (set_color green) (whoami) " " (set_color blue) (prompt_pwd) (set_color red) (fish_git_prompt) " " (set_color normal)"→ "
+    echo -s (set_color green) $USER(set_color normal) "@" (set_color green)$hostname " " (set_color blue) (prompt_pwd) (set_color red) (fish_git_prompt) " " (set_color normal)"→ "
 end
