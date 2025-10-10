adb98f30a02590d91a45c344e007221ee4c9616b
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-10-10 21:26:27 +0300

    Update Fish $PATH resolution

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,18 @@ if status is-interactive
     set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_function_path
     set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 
+    if test -d $HOME/bin
+        set PATH "$HOME/bin:$PATH"
+    end
+
+    if test -d $HOME/.local/bin
+        set PATH "$HOME/.local/bin:$PATH"
+    end
+
+    if test -d $HOME/$HOME/.local/scripts
+        set PATH "$HOME/.local/scripts:$PATH"
+    end
+
     if test -x $HOME/.local/bin/mise
         mise activate fish | source
     end
