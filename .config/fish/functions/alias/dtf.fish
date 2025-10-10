46b4e2e49b758c05ddd9779666c95c7848ea67b0
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-10-10 19:32:43 +0300

    Add dtf alias

---
+++ .config/fish/functions/alias/dtf.fish
@@ -0,0 +1,3 @@
+function dtf --wraps='git --git-dir=/home/gintaras/.dotfiles --work-tree=/home/gintaras' --description 'alias dtf=git --git-dir=/home/gintaras/.dotfiles --work-tree=/home/gintaras'
+    git --git-dir=/home/gintaras/.dotfiles --work-tree=/home/gintaras $argv
+end
