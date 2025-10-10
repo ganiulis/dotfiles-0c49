2d852c04eb6b3a5789637648793708bd82cc7c4a
ganiulis <91011496+ganiulis@users.noreply.github.com>
2025-10-10 10:39:57 +0300

    Update Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+# Allows for nested function/completion directories
 if status is-interactive
     set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_function_path
+    set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 end
