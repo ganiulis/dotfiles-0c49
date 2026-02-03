758849da7ef3e87dd75d1b168583b98066230e4e
ganiulis <91011496+ganiulis@users.noreply.github.com>
2026-02-03 18:01:16 +0000

    Fix treesitter config

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
   opts = {
+    lazy = false,
     sync_install = true,
     ensure_installed = {
       "c",
@@ -15,33 +16,14 @@ return {
       "markdown",
       "markdown_inline",
     },
-    highlight = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<CR>",
-        scope_incremental = "<CR>",
-        node_incremental = "<TAB>",
-        node_decremental = "<S-TAB>",
-      },
-    },
   },
   config = function(_, opts)
     if vim.fn.executable("nixos-rebuild") == 1 then
-      opts["ensure_installed"] = {}
-      opts["auto_install"] = false
-      opts["sync_install"] = false
+      opts.ensure_installed = {}
+      opts.auto_install = false
+      opts.sync_install = false
     end
 
     require("nvim-treesitter").setup(opts)
-
-    if vim.fn.executable("nixos-rebuild") == 1 then
-      vim.api.nvim_create_autocmd({ "FileType" }, {
-        pattern = "markdown",
-        callback = function() vim.treesitter.start() end,
-      })
-    end
   end,
 }
---
+++ .config/nvim/lua/plugins/treesitter-modules.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          scope_incremental = "<CR>",
+          node_incremental = "<TAB>",
+          node_decremental = "<S-TAB>",
+        },
+      },
+    },
+  },
+}
